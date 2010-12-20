395da83cabdaedc458e2febc1e782e5188137e8a
Jari Rosti <jari.rosti@gmail.com>
2010-12-20 13:21:27 +0200

    ui draft

--- lumikki.js
+++ lumikki.js
@@ -1,3 +1,17 @@
 $(document).ready(function() {
-    
-})
+	$('#logstatus').submit(function() {
+		getStatus();
+		return false;
+	    });
+    });
+
+
+$(function() {
+	$(".css-tabs:first").tabs(".css-panes:first > div");
+});
+
+function getStatus() {
+    $.get("http://localhost:8080/status", function(data) {
+		alert(data.status);
+    }, "json");
+}
--- server.py
+++ server.py
@@ -11,7 +11,25 @@ PORT = 8080
 class TestHandler(SimpleHTTPServer.SimpleHTTPRequestHandler):
     """The test example handler."""
     def do_GET(self):
-        self.wfile.write(json.dumps({'status':100}))
+        print "Got request:" + self.path
+        self.send_response(200)
+        if self.path.endswith(".html") or self.path.endswith(".js"): 
+            data = open(self.path[1:], 'r').read()
+            if self.path.endswith(".html"): 
+                self.send_header("Content-type:", "text/html")
+            else:
+                self.send_header("Content-type:", "text/javascript")
+            self.send_header("Content-length", str(len(data)))
+            self.end_headers()
+            self.wfile.write(data)
+        elif self.path.endswith("status"): 
+            self.send_header("Content-type", "application/javascript")
+            s = json.dumps({'status':100})
+            self.send_header("Content-length", str(len(s)))
+            self.end_headers()
+            self.wfile.write(s)
+        else:
+            self.send_error(404)
 
 def start_server():
     """Start the server."""
