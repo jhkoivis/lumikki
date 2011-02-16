5c69de88a306f53cdd34ca2384897d3bf4bdabc4
Simo Tuomisto <st.tuomisto@gmail.com>
2011-02-16 20:29:39 +0200

    New ttm.

--- webroot/cmd/ttm.py
+++ webroot/cmd/ttm.py
@@ -6,7 +6,7 @@ from lumilib import *
 
 timeout = 3
 
-statusResponses = dict({ "000":"Cannot connect to anything"
+statusResponses = { "000":"Cannot connect to anything"
        , "010":"Server sent unknown status"
        , "020":"Server response did not contain status"
        , "100":"Disabled from server."
@@ -17,40 +17,62 @@ statusResponses = dict({ "000":"Cannot connect to anything"
        , 200:"210"
        , "220":"Measuring"
        , "230":"System is busy, please try again in a while." 
-})
+}
 
-def connectAndSendStart():
-    c = conf()
-    command = "http://%s:%s/ttmStart" % (c.get('ttm_ip'),c.get('ttm_port'))
-    response = connect(command)
-    return getResponseString(response)
-    
-def connectAndSendStop():
-    c = conf()
-    command = "http://%s:%s/ttmStop" % (c.get('ttm_ip'),c.get('ttm_port'))
-    response = connect(command)
-    return getResponseString(response)
 
-def connectAndGetStatus():
-    c = conf()
-    command = "http://%s:%s" %(c.get('ttm_ip'),c.get('ttm_port'))
-    response = connect(command)
-    return statusResponses[response]
+def connectToCommand(command, data=None):
     
-def connectAndSetRamp():
+    command_root = "/lumikki/csm_lumikki_instron_"
+
+    return createUrlAndConnect(command_root, data)
+
+def createUrlAndConnect(command, data=None):
     c = conf()
-    command = "http://%s:%s/ttmRamp" % (c.get('ttm_ip'),c.get('ttm_port'))
-    data = { rampRate:      c.get('ttm_ramprate')
-            ,rampHeight:    c.get('ttm_rampheight')
-            ,channel:       c.get('ttm_channel')
-            }
-    response = connect(command, data)
-    return getResponseString(response)
+    basic_url = "http://%s:%s" % (c.get('ttm_ip'),c.get('ttm_port'))
+    url = basic_url + command
+    if data != None:
+        url += "?%s" % urlencode(data)
+    return connect(url)
+    
+def connect(url):
+    connection = urlopen(url, timeout=timeout)
+    response = connection.getcode()
+    return response
 
 def getResponseString(response):
     return responses[response]
+
+def connectAndStartLogging():
+    c = conf()
+    data = {"expId": c.get('g_measurementid')}
+    response = connectToCommand("startLogging", data)
+    return getResponseString(response)
+
+def connectAndStopLogging():
+    response = connectToCommand("stopLogging")
+    return getResponseString(response)
     
-def connect(command, data=None):
-    connection = urlopen(command, data=data,timeout=3)
-    response = connection.getcode()
-    return response
+def connectAndInitRamp():
+    c = conf()
+    data = { "rampRate":      c.get('ttm_ramprate')
+            ,"rampAmplitude": c.get('ttm_rampamplitude')
+            }
+    response = connectToCommand("initRamp", data)
+    return getResponseString(response)
+
+def connectAndStartRamp():
+    response = connectToCommand("startRamp")
+    return getResponseString(response)
+
+def connectAndMoveToSetPoint():
+    c = conf()
+    data = { "setpoint":     c.get('ttm_setpoint')
+            ,"setpointTime": c.get('ttm_setpointtime')
+            }
+    response = connectToCommand("moveToSetpoint", data)
+    return getResponseString(response)
+
+def connectAndGetStatus():
+    # This needs more sophisticated ideas in order to recognize the measuring
+    response = createUrlAndConnect("")
+    return statusResponses[response]
