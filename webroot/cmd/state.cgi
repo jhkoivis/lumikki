0f84fc9fed21abed1985f658daaac2a4b37a143b
Simo Tuomisto <st.tuomisto@gmail.com>
2011-03-15 16:06:10 +0200

    Added connect-and disconnect-buttons.

---
+++ webroot/cmd/state.cgi
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+
+from lumilib import *
+from config import conf, configurationMap
+
+command_id = 1
+try: 
+    c = conf()
+    jsonMessage = get_json()
+    command_id = jsonMessage['id']
+    del jsonMessage['id']
+    put_json(c.getAndSetCurrent(jsonMessage))
+except Exception as e:
+    raise
+    put_json({'id':command_id, 'st':1, 'msg':'State.cgi failed [' + str(e) + ']'})
---
+++ webroot/cmd/ttm.py
@@ -0,0 +1,87 @@
+from urllib2 import urlopen, URLError
+from urllib import urlencode
+from httplib import responses
+from config import conf
+from lumilib import *
+
+timeout = 3
+
+statusResponses = { "000":"Cannot connect to anything"
+       , "010":"Server sent unknown status"
+       , "020":"Server response did not contain status"
+       , "100":"Disabled from server."
+       , "110":"Target machine did not respond"
+       , "120":"Server received malformed command."
+       , 403:"122"
+       , "200":"Target available, values not set"
+       , 200:"210"
+       , "220":"Measuring"
+       , "230":"System is busy, please try again in a while." 
+}
+
+def connectToCommand(command, data=None):
+    
+    command_root = "/lumikki/csm_lumikki_instron_"
+    
+    return createUrlAndConnect(command_root + command, data)
+
+def createUrlAndConnect(command, data=None):
+    c = conf()
+    basic_url = "http://%s:%s" % (c.get('ttm_ip'),c.get('ttm_port'))
+    url = basic_url + command
+    if data != None:
+        url += "?%s" % urlencode(data)
+    return connect(url)
+    
+def connect(url):
+    connection = urlopen(url, timeout=timeout)
+    response = connection.getcode()
+    return response
+
+def getResponseString(response):
+    return responses[response]
+
+def getStatusResponseString(response):
+    return statusResponses[response]
+
+def connectAndStartLogging():
+    c = conf()
+    data = {"expId": c.get('g_measurementid')}
+    response = connectToCommand("startLogging", data)
+    return getResponseString(response)
+
+def connectAndStopLogging():
+    response = connectToCommand("stopLogging")
+    return getResponseString(response)
+    
+def connectAndInitRamp():
+    c = conf()
+    data = { "rampRate":      c.get('ttm_ramprate')
+            ,"rampAmplitude": c.get('ttm_rampamplitude')
+            }
+    response = connectToCommand("initRamp", data)
+    return getResponseString(response)
+
+def connectAndStartRamp():
+    response = connectToCommand("startRamp")
+    return getResponseString(response)
+
+def connectAndStop():
+    response = connectToCommand("stop")
+    return getResponseString(response)
+
+def connectAndMoveToSetPoint():
+    c = conf()
+    data = { "setpoint":     c.get('ttm_setpoint')
+            ,"setpointTime": c.get('ttm_setpointtime')
+            }
+    response = connectToCommand("moveToSetpoint", data)
+    return getResponseString(response)
+
+def connectAndGetStatus():
+    # This needs more sophisticated ideas in order to recognize the measuring
+    try:
+        response = createUrlAndConnect("")
+    except URLError:
+        return "110"
+    return getStatusResponseString(response)
