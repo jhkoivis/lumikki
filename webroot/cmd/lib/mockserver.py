355fedb6c323faef990c919437d90278552a0932
Simo Tuomisto" <st.tuomisto@gmail.com>
2011-04-12 10:46:33 +0300

    Mockserver and mockdevices.

---
+++ webroot/cmd/lib/mockserver.py
@@ -0,0 +1,49 @@
+from BaseHTTPServer import HTTPServer
+from threading import Thread
+from SocketServer import ThreadingMixIn
+from mockdevices import MockCamHandler, MockTTMHandler
+from urllib2 import urlopen
+
+class MockServer:
+        
+    def __init__(self):
+    
+        self.devicemap = {
+                          MockCamHandler:('localhost',40001),
+                          MockTTMHandler:('localhost',40002)
+                          }
+        self.devices = dict()
+        for handler, address in self.devicemap.items():
+            device = MockDevice(address, handler)
+            self.devices[device] = Thread(target=device.serve)
+            
+    def run(self):
+        try:
+            for device, thread in self.devices.items():
+                thread.daemon = False
+                thread.start()
+        except AssertionError:
+            raise Exception("Run not possible, server destroyed.")
+            
+    def stop(self):
+        for device, thread in self.devices.items():
+            if thread.isAlive:
+                device.stop_server()
+                device.socket.close()
+                device.server_close()
+
+class MockDevice(HTTPServer,ThreadingMixIn):
+
+    def __init__(self, address, handler):
+        self.stop = False
+        HTTPServer.__init__(self, address, handler)
+    
+    def serve(self):
+        while True:
+            self.handle_request()
+            if self.stop:
+                break
+    
+    def stop_server(self):
+        self.stop = True
+        urlopen("http://%s:%s" % self.server_address)
