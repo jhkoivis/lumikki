137cf00d90c368d1f2f62d09643c2ad2cd6b4fa3
mmyntti <mmyntti@localhost>
2011-06-13 14:31:56 +0300

    Fixed emmental3

--- tools/emmental3.sh
+++ tools/emmental3.sh
@@ -6,12 +6,12 @@
 # - create a pipe from controller  to 
 #
 # hugoputket
-username=jko
+username=$1
 local_lumikki_ssh=7780
 local_ttm_rdesktop=7781
 
 firewall=hugo.hut.fi
-lumikki=130.233.202.249
+lumikki=erin.hut.fi
 ttm=10.0.0.10
 
 p_rdesktop=3389
@@ -20,7 +20,7 @@ p_ssh=22
 middlePort1=52759
 
 
-ssh -ftX $firewall ssh -fN -L $middlePort1:$ttm:$p_rdesktop sauna@$lumikki
+ssh -ftX $firewall ssh -fN -L $middlePort1:$ttm:$p_rdesktop $username@$lumikki
 
 ssh -fN -L $local_lumikki_ssh:$lumikki:$p_ssh $username@$firewall # veranta
 ssh -fN -L $local_ttm_rdesktop:localhost:$middlePort1 $username@$firewall  # veranta
