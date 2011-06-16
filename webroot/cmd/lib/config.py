b86ea987b19ef29f31cebd5b22b31750ee00d845
jko <jko@localhost>
2011-06-16 23:28:28 +0300

    fixed and verified ~ as a directory added configuration options to config.py

--- webroot/cmd/lib/config.py
+++ webroot/cmd/lib/config.py
@@ -1,39 +1,10 @@
 import memcache
 
-class conf:
-    def __init__(self):
-        self.mc = memcache.Client(['127.0.0.1:11211'], debug=0)
+# TODO: load configuration map from a file
 
-    def getAndSetCurrent(self, inputMap):
-        retDict = dict()
-        for key in configurationMap: 
-            inKeys = inputMap.keys()
-            if key in inKeys:
-                self.set(key, inputMap[key])
-            retDict[key] = self.get(key)
-        return retDict
-
-    def get(self, key):
-        value = self.mc.get(key)
-        if not value: 
-            svalue = configurationMap[key]
-            if svalue:
-                self.mc.set(key,svalue)
-            return svalue
-        else:
-            return value
-
-    def set(self, key, value):
-        self.mc.set(key,value)
-
-class staticglobals: 
-    ttm = 0
-    ae = 1
-    cam = 2
-    ir = 3
-
-configurationMap = {
-    'g_active':[False, False, False, False]
+configurationMap = {'g_logdir'        : '~'
+    , 'g_memcacheAddr'	: '127.0.0.1:50001'
+    , 'g_active':[False, False, False, False]
     , 'g_measurementid'     : 'default'
     , 'g_timestamp'     : ''
     , 'cam_parentfolder' : r'F:\data'
@@ -67,3 +38,37 @@ configurationMap = {
     , 'ir_storevalue' : '0'
     , 'ir_recordformat' : '2'
 }
+
+class conf:
+    def __init__(self):
+        self.mc = memcache.Client([configurationMap['g_memcacheAddr']], debug=0)
+
+    def getAndSetCurrent(self, inputMap):
+        retDict = dict()
+        for key in configurationMap: 
+            inKeys = inputMap.keys()
+            if key in inKeys:
+                self.set(key, inputMap[key])
+            retDict[key] = self.get(key)
+        return retDict
+
+    def get(self, key):
+        value = self.mc.get(key)
+        if not value: 
+            svalue = configurationMap[key]
+            if svalue:
+                self.mc.set(key,svalue)
+            return svalue
+        else:
+            return value
+
+    def set(self, key, value):
+        self.mc.set(key,value)
+
+class staticglobals: 
+    ttm = 0
+    ae = 1
+    cam = 2
+    ir = 3
+
+
--- webroot/cmd/state.cgi
+++ webroot/cmd/state.cgi
@@ -2,14 +2,16 @@
 
 from lib.lumilib import *
 from lib.config import conf, configurationMap
+import os
 
 command_id = 1
 try: 
     c = conf()
     jsonMessage = get_json()
     command_id = jsonMessage['id']
-    
-    outFile = open('/tmp/lumikki.log', 'a')
+   
+    path = os.path.expanduser(c.get('g_logdir'))
+    outFile = open(path + '/lumikki.log', 'a')
     for key, value in jsonMessage.items():
     	outFile.write(str(key) + str(value))
     outFile.close()
@@ -18,5 +20,5 @@ try:
     put_json(c.getAndSetCurrent(jsonMessage))
 
 except Exception as e:
-    raise
     put_json({'id':command_id, 'st':1, 'msg':'State.cgi failed [' + str(e) + ']'})
+    raise
