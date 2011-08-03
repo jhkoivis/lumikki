c90a1e360945f777e5c8eb94213d01a67727eaca
jko <jko@localhost>
2011-08-03 18:13:56 +0300

    moved all user dependent configurations to conf/user.conf.orig

--- runmockserver.bash
+++ runmockserver.bash
@@ -1,4 +1,11 @@
 #!/bin/bash
 
 . lumikki.env
+
+# load environment similar to conf/cgi.conf
+
+export LUMIKKI_PYTHON_UIDEFAULTS=$lumikki_python_uidefaults
+export LUMIKKI_MEMCACHE_IP=$lumikki_memcache_ip
+export LUMIKKI_MEMCACHE_PORT=$lumikki_memcache_port
+
 python webroot/cmd/lib/mockserver.py
