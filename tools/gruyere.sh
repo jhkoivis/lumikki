f895ffb2e741991fc1f8e1efae248c08b8ce6734
jko <jko@localhost>
2011-08-18 18:02:13 +0300

    modified rdesktop command

--- tools/gruyere.sh
+++ tools/gruyere.sh
@@ -62,23 +62,30 @@ middlePort_cam=$[42333 + $id]
 middlePort_ir=$[43333 + $id]
 middlePort_ae=$[44333 + $id]
 
-# create pipes from erin to device computer
+echo
+echo echo ttm:
 echo ssh -ftX $firewall ssh -fN -L $middlePort_ttm:$ttm:$p_rdesktop 	$username@$erin
+echo ssh -fN -L $local_ttm_rdesktop:localhost:$middlePort_ttm $username@$firewall 
+echo
+echo echo cam:
 echo ssh -ftX $firewall ssh -fN -L $middlePort_cam:$cam:$p_rdesktop 	$username@$erin
+echo ssh -fN -L $local_cam_rdesktop:localhost:$middlePort_cam $username@$firewall 
+echo
+echo echo ir:
 echo ssh -ftX $firewall ssh -fN -L $middlePort_ir:$ir:$p_rdesktop 	$username@$erin
-echo ssh -ftX $firewall ssh -fN -L $middlePort_ae:$ae:$p_rdesktop 	$username@$erin
-
-# create pipes from localhost to firewall
-echo ssh -fN -L $local_ttm_rdesktop:localhost:$middlePort_ttm $username@$firewall  
-echo ssh -fN -L $local_cam_rdesktop:localhost:$middlePort_cam $username@$firewall  
 echo ssh -fN -L $local_ir_rdesktop:localhost:$middlePort_ir $username@$firewall 
-echo ssh -fN -L $local_ae_rdesktop:localhost:$middlePort_ae $username@$firewall  
+echo
+echo echo ae:
+echo ssh -ftX $firewall ssh -fN -L $middlePort_ae:$ae:$p_rdesktop 	$username@$erin
+echo ssh -fN -L $local_ae_rdesktop:localhost:$middlePort_ae $username@$firewall
+echo
 
 # echo instructions
-echo echo ttm: rdesktop localhost:$local_ttm_rdesktop
-echo echo cam: rdesktop localhost:$local_cam_rdesktop
-echo echo ir: rdesktop localhost:$local_ir_rdesktop
-echo echo ae: rdesktop localhost:$local_ae_rdesktop
+echo echo ttm: rdesktop localhost:$local_ttm_rdesktop -k Fi-fi -f -rdisk:home=/home/$username/
+echo echo cam: rdesktop localhost:$local_cam_rdesktop -k Fi-fi -f -rdisk:home=/home/$username/
+echo echo ir: rdesktop localhost:$local_ir_rdesktop -k Fi-fi -f -rdisk:home=/home/$username/
+echo echo ae: rdesktop localhost:$local_ae_rdesktop -k Fi-fi -f -rdisk:home=/home/$username/
+
 
 
 
