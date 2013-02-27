af0cae990334aea26f59673fce97f170ebdf7911
jko <jko@localhost>
2013-02-27 16:32:58 +0200

    working version after new camera installation

--- webroot/cmd/lib/cam.py
+++ webroot/cmd/lib/cam.py
@@ -1,111 +1,47 @@
-import LabVIEW
-import getpass
+
 from config import conf
 from lumilib import *
 from json import loads
-from urllib2 import urlopen, URLError
-from urllib import urlencode
+from json import dumps
+import device
 
-#def connectAndSendStart():
 def start():
+    '''
+    Excecute on run click.
+    '''
     c = conf()
-    return connectAndSend(c, createStartMessage(c))
+    keyList = c.getAllKeys()
+    keyList.sort()
+    data = {}
+    
+    for key in keyList:
+        sys.stderr.write(str(key[0:5])+ '\n')
+        if (key[0:2] == 'g_') or (key[0:4] == 'cam_'):
+            # lists and objects are not implemented in labview part
+            # ',' and ':' are thus forbidden
+            value = str(c.get(key))
+            if ',' in dumps(value): continue
+            if ':' in dumps(value): continue
+            if value.__class__ == [].__class__: continue
+            data[key] = value
 
-def connectAndStart():
-#def start():
-    c = conf()
-    data = {"offsetY":      c.get('cam_offsety'),
-            "offsetX":      c.get('cam_offsetx'),
-            "fps":      c.get('cam_rawfps'),
-            "width":      c.get('cam_width'),
-            "height":      c.get('cam_height'),
-            "testName":      c.get('g_measurementid'),
-            "packSize":      c.get('cam_packetsize'),
-            "index":      c.get('cam_index'),
-            "exposure":      c.get('cam_exposure'),
-            "parentFolder": c.get('cam_parentfolder')
+    return device.labViewCommandPostJson(c.get('cam_global_ip'),
+                                        c.get('cam_global_port'),
+                                        c.get('cam_global_service'),
+                                        'cam_start',
+                                        data=data)
 
-            }
-    return connectAndStartCam(data)
-
-def connectAndStartCam(data):
-    c = conf()
-    basic_url = "http://%s:%s/camera/manualCamera" % (c.get('cam_ip'),c.get('cam_port'))
-    url = basic_url
-    if data != None:
-        url += "?%s" % urlencode(data)
-            
-    connection = urlopen(url, timeout=float(c.get('g_timeout')))
-    return connection
-
-#def connectAndSendStop():
 def stop():
     c = conf()
-    return connectAndSend(c, createStopMessage(c))
+    return device.labViewCommandGeneral(c.get('cam_global_ip'), 
+                    c.get('cam_global_port'), 
+                    c.get('cam_global_service'),
+                    'cam_stop')
 
-#def connectAndGetStatus():
 def status():
     c = conf()
-    responce = connectAndSend(c, createStatusMessage(c))
-	# TODO: this is a hack that will be fixed on 
-	# device side during summer 2011
-    rDict = {}
-    splittedResponce = responce.split('\n')
-    for item in splittedResponce:
-        splittedItem = item.split(':',1)
-        rDict[splittedItem[0].strip()] = splittedItem[1].strip()
-    if not rDict.has_key('Errors'):
-        return 240
-    if not rDict['Errors'] == '':
-        return 240
-    if not rDict.has_key('Satus'):
-        return 240
-    if rDict['Satus'].find('CAMERA IS STOPPED') >= 0:
-        return 210
-    if rDict['Satus'].find('CAMERA IS ON') >= 0:
-        return 220
-    if len(responce) == 0:
-        return 240 
-    return responce    
-
-def connectAndSend(c,message):
-    log(message);
-    ret = connectAndSendMessage(c, message)
-    return ret
-
-def createStatusMessage(c):
-    return "status?"
-
-def createStopMessage(c):
-    return "M(2)"
-
-def createStartMessage(c):
-    """
-    return start message
-    """
-    message =  "M(1)"
-    message += "S(" + c.get('cam_packetsize') + ")"
-    message += "F(" + c.get('cam_rawfps')     + ")"
-    message += "U(" + getpass.getuser()     + ")"
-    message += "W(" + c.get('cam_width')      + ")"
-    message += "H(" + c.get('cam_height')     + ")"
-    message += "X(" + c.get('cam_offsetx')    + ")"
-    message += "Y(" + c.get('cam_offsety')    + ")"
-    message += "E(" + c.get('cam_exposure')   + ")"
-    mid = "%s-%s" % (c.get('g_measurementid'), c.get('g_timestamp'))
-    message += "T(" + c.get('cam_parentfolder') + '\\' + mid + '\\' + mid + ")"
-    message += "I(" + "0"                   + ")"
-    print message
-    return message
-
-def connectAndSendMessage(c,message):
-    """
-    Connects to labview and sends message
-    returns returnmessage. Passes throw e.g. ValueError
-    and other expceptions, which are handled by the upper level.
-    """
-    LabVIEW.connect(c.get('cam_ip'), int(c.get('cam_port')))
-    messageout = LabVIEW._passCommand(message)
-    LabVIEW.disconnect()
-    return messageout
+    return device.labViewCommandGeneral(c.get('cam_global_ip'), 
+                                        c.get('cam_global_port'),
+                                        c.get('cam_global_service'), 
+                                        'cam_status')
 
--- webroot/cmd/lib/device.py
+++ webroot/cmd/lib/device.py
@@ -1,42 +1,12 @@
 from urllib2 import urlopen, URLError
 from urllib import urlencode
+import httplib, urllib
 from config import conf
 from lumilib import *
 from json import loads
+from json import dumps
+from base64 import b64encode
 import sys
-
-
-def labViewCommand(service, command, data=None):
-    '''
-        General connection command for labview devices.
-
-        Connects to a certain ip, port, labview service and 
-        labview service command. These variables depend on 
-        the device installation (taken from uidefaults.conf).
-
-        Data includes the command (stringyfied to json format)
-        posted via http-post.
-    '''
-    c = conf()
-    url = "http://%s:%s/%s/%s" % (  c.get(service + '_ip'),
-                                    c.get(service + '_port'),
-                                    service,
-                                    command)
-    if data != None:
-        url += "?%s" % urlencode(data)
-    try:
-        connection = urlopen(url, timeout=int(c.get('g_timeout')))
-    except:
-        sys.stderr.write(url)
-        raise
-     
-    # connection must be a string, 
-    # urlopen may return a request object if device is not present
-    ccn = connection.__class__.__name__
-    if not (ccn == "str" or ccn == "unicode"):
-        connection = '240' # not ready
-    
-    return connection
 
 def labViewCommandGeneral(ip, port, service, command, data=None):
     '''
@@ -49,6 +19,8 @@ def labViewCommandGeneral(ip, port, service, command, data=None):
         Data includes the command (stringyfied to json format)
         posted via http-post.
     '''
+    sys.stderr.write("cam from device.py")
+    sys.stderr.flush()
     c = conf()
     url = "http://%s:%s/%s/%s" % (  ip,
                                     port,
@@ -56,6 +28,7 @@ def labViewCommandGeneral(ip, port, service, command, data=None):
                                     command)
     if data != None:
         url += "?%s" % urlencode(data)
+    sys.stderr.write(url)
     try:
         connection = urlopen(url, timeout=int(c.get('g_timeout')))
     except:
@@ -69,3 +42,85 @@ def labViewCommandGeneral(ip, port, service, command, data=None):
         connection = '240' # not ready
     
     return connection
+
+def labViewCommandPostJson(ip, port, service, command, data=None):
+    '''
+        General connection command for labview devices.
+
+        Connects to a certain ip, port, labview service and 
+        labview service command. These variables depend on 
+        the device installation (taken from uidefaults.conf).
+
+        Data includes the command (stringyfied to json format)
+        posted via http-post.
+    '''
+    c = conf()
+    #url = "http://%s:%s/%s/%s/" % (  ip,
+    #                                port,
+    #                                service,
+    #                                command)
+    #if data != None:
+    #    url += "?postdata=%s" % b64encode(dumps(data))
+    #sys.stderr.write(url)
+    ipPort = "%s:%s" % (ip, port)
+    serviceCommand = "/%s/%s/" % (service, command)
+    sys.stderr.write(ipPort + ' ' + serviceCommand + ' \n')
+    
+    #h = urlopen(ipPort)
+    params = urllib.urlencode({'postdata': dumps(data)})
+    headers = {"Content-type": "application/x-www-form-urlencoded",
+               "Accept": "text/plain"}
+    conn = httplib.HTTPConnection(ipPort)
+    conn.request("POST", serviceCommand, params, headers)
+    connection = conn.getresponse().read()
+
+    #connection = h.request('POST', serviceCommand, data)
+    
+#    try:
+#        connection = urlopen(url, timeout=int(c.get('g_timeout')))
+#    except:
+#        sys.stderr.write(url)
+#        raise
+     
+    # connection must be a string, 
+    # urlopen may return a request object if device is not present
+#    ccn = connection.__class__.__name__
+#    if not (ccn == "str" or ccn == "unicode"):
+#        connection = '240' # not ready
+    
+    sys.stderr.write(connection + '\n')
+    
+    return connection
+
+#def labViewCommand(service, command, data=None):
+#    '''
+#        General connection command for labview devices.
+#
+#        Connects to a certain ip, port, labview service and 
+#        labview service command. These variables depend on 
+#        the device installation (taken from uidefaults.conf).
+#
+#        Data includes the command (stringyfied to json format)
+#        posted via http-post.
+#    '''
+#    c = conf()
+#    url = "http://%s:%s/%s/%s" % (  c.get(service + '_ip'),
+#                                    c.get(service + '_port'),
+#                                    service,
+#                                    command)
+#    if data != None:
+#        url += "?%s" % urlencode(data)
+#    sys.stderr.write(url)
+#    try:
+#        connection = urlopen(url, timeout=int(c.get('g_timeout')))
+#    except:
+#        sys.stderr.write(url)
+#        raise
+#     
+#    # connection must be a string, 
+#    # urlopen may return a request object if device is not present
+#    ccn = connection.__class__.__name__
+#    if not (ccn == "str" or ccn == "unicode"):
+#        connection = '240' # not ready
+#    
+#    return connection
