478f6a6c0712586cabff3bf8d2cb0a2dc51f2891
jhkoivis <jhkoivis@localhost>
2014-02-10 15:40:18 +0200

    Minor edit, added debug echo

--- runuserserver.bash
+++ runuserserver.bash
@@ -1,6 +1,8 @@
 
 . lumikki.env
 
+echo memcached -p $lumikki_memcache_port 
 memcached -p $lumikki_memcache_port &
 
+echo lighttpd -f $lumikki_config_path/lighttpd.conf -D
 lighttpd -f $lumikki_config_path/lighttpd.conf -D
